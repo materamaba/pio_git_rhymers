7d3a08cec608fe7c13d7303d999a69c74bfaf9db
materamaba <materamaba@gmail.com>
2026-04-12 20:20:36 +0200

    15. Drobne poprawki i zaliczone testy

--- src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
+++ src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
@@ -3,6 +3,8 @@ package edu.kis.vh.nursery;
 import org.junit.Assert;
 import org.junit.Test;
 
+// Projekt jest poprawny i przechodzi walidacje testami jednostkowymi
+
 public class RhymersJUnitTest {
 
     @Test
