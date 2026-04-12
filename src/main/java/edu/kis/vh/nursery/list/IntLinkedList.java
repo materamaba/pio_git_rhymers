e27aea9174d4db2111b20996ebc7e29e4700c228
materamaba <materamaba@gmail.com>
2026-04-12 19:24:49 +0200

    14. Zamiana klasy Node na klasę wewnętrzną

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -6,6 +6,21 @@ public class IntLinkedList {
     private Node last;
     private int i;
 
+    private class Node {
+
+        private int value;
+        private Node prev, next;
+
+        public Node(int i) {
+            setValue(i);
+        }
+
+        private void setValue(int value) {
+            this.value = value;
+        }
+
+    }
+
     public void push(int i) {
         if (last == null)
             last = new Node(i);
